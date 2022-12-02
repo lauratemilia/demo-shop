735408eede6b13db0fa77dc45f55876910880c08
Laura Toporas <laura.toporas@pss.ro>
2022-12-02 15:40:56 +0200

    added product file, category and cart pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,6 +9,7 @@ function Header() {
         <div>
             <Link to='/'>Home</Link>
             <Link to='/login'>Login</Link>
+            <Link to='/register'>Register</Link>
         </div>
     );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,10 +4,7 @@ import {Link} from 'react-router-dom';
 function Login() {
     return(
         <div>
-            <h1>Login</h1>
-            {/* Din Login Am vrea sa ajungem cu un click in Register sau in Home => avem nevoie de componenta Link!*/}
-            {/* Link este echivalentul unui tag de tip "a", doar ca acesta nu va reincarca pagina catre care directioneaza */}
-            {/* In atributul "to" punem ruta catre care vrem sa navigam. */}
+            <h1>Login</h1>            
             <Link to='/'>Home</Link>
             <Link to='/register'>Register</Link>
         </div>
